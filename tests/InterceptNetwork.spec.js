ef47f1491663f5de9fc8570de022506e0111cc9f
Ravindu-96 <nethmina.kar@gmail.com>
2026-02-27 12:26:42 +0530

    Add security test for unauthorized order access

--- tests/InterceptNetwork.spec.js
+++ tests/InterceptNetwork.spec.js
@@ -2,6 +2,7 @@
 // It logs in to the application, saves the session state, and then uses that state in a subsequent test to access authenticated pages without needing to log in again.
 
 import { test, expect } from "@playwright/test";
+import { url } from "node:inspector";
 
 let webContext;
 let emptyOrdersResponse = { data: [], message: "No Orders" }
@@ -22,7 +23,9 @@ test.beforeAll(async ({ browser }) => {
 
 });
 
-test.only("No Orders in Cart", async () => {
+// This test verifies that when there are no orders in the cart, the application correctly displays a "No Orders" message.
+//  It mocks the API response to return an empty orders list and checks the UI for the appropriate message.
+test("No Orders in Cart", async () => {
     const page1 = await webContext.newPage();
     await page1.goto("https://rahulshettyacademy.com/client/#/dashboard/dash");
 
@@ -42,4 +45,23 @@ test.only("No Orders in Cart", async () => {
 
     // Verify that the "No Orders" message is displayed in the UI
     await expect(page1.locator("div .ng-star-inserted")).toBeVisible();
+});
+
+
+// This test verifies that users cannot access order details of other users by manipulating the API request.
+test.only("Security Check for view other users orders", async () => {
+    const page2 = await webContext.newPage();
+    await page2.goto("https://rahulshettyacademy.com/client/#/dashboard/dash");
+
+    await page2.locator("[routerlink*='myorders']").click();
+
+    // Route page for order details API and attempt to access another user's order details
+    page2.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-details?id=*", route => {
+        route.continue({
+            url: "https://rahulshettyacademy.com/api/ecom/order/get-orders-details?id=12345", // Attempt to access another user's order details
+        });
+    });
+    await page2.locator("button:has-text('View')").first().click();
+    await expect(page2.locator(".blink_me")).toBeVisible();
+
 });
